b14b5840f376212a94bd2788e24cb51f6f87c513
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-26 20:14:22 -0400

    Additional testing of checkOn feature - looking good!

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,21 @@ class Main {
         Owner owner = new Owner("Nikki", "28 Richard Rd", "1112224444");
         ken.addOwner(owner);
         Pet telly = new Cat("Telly");
+        Pet charlie = new Dog("Charlie");
         owner.addPet(telly);
+        owner.addPet(charlie);
         telly.setMealsPerDay(3);
         telly.setWalksPerDay(1);
+        charlie.setWalksPerDay(2);
+        charlie.setMealsPerDay(3);
+
+        telly.feedPet();
+        telly.walkPet();
+        telly.feedPet();
+
+        charlie.walkPet();
+        charlie.feedPet();
+
 
         String purpose = ui.getPurpose();
         if(purpose.equals("1")){
